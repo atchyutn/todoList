2e0de1257f80ec9ded328d2cf22298e2d15b5f14
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-04-15 23:39:42 +0530

    Adds new task form

---
+++ components/AddTask.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+
+const AddTask = () => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? "padding" : "height"}
+      style={styles.textInputWrapper}
+    >
+      <TextInput
+        placeholder='Add your task'
+        style = {styles.textInput}
+      />
+      <TouchableOpacity>
+        <View style={styles.addBtnWrapper}>
+          <Text style={styles.addBtn}>
+            >
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  )
+};
+
+const styles = StyleSheet.create({
+  textInputWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  
+  textInput: {
+    borderColor: '#36C2F9',
+    borderWidth: 1,
+    borderRadius: 50,
+    height: 50,
+    width: 250,
+    padding: 10,
+    backgroundColor: '#FFFFFF'
+  },
+  addBtnWrapper: {
+    borderColor: '#36C2F9',
+    borderWidth: 1,
+    borderRadius: 100,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#36C2F9'
+  },
+  addBtn: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 40,
+    color: 'white',
+    fontWeight: 'bold',
+    paddingTop: 0
+  }
+
+});
+
+
+export default AddTask;
--- components/Task.js
+++ components/Task.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import AddTask from './AddTask';
 
 const Task = (props) => {
   return (
-    <View style={styles.item}>
-      <View style={styles.itemLeft}>
-        <View style={styles.markDoneBtn}></View>
-        <Text style={styles.itemText}>{props.text}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.item}>
+        <View style={styles.itemLeft}>
+          <View style={styles.markDoneBtn}></View>
+          <Text style={styles.itemText}>{props.text}</Text>
+        </View>
+        <View style={styles.circle}></View>
       </View>
-      <View style={styles.circle}></View>
+      <View style={styles.addTask}>
+        {/* Add new task */ }
+        <AddTask></AddTask>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },  
   item: {
+    flex: 1,
     backgroundColor: '#EB984E',
     padding: 15,
     borderRadius: 10,
@@ -22,8 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 10,
-    marginTop: 10
-
+    marginTop: 20,
+    minHeight: 80
   },
   itemLeft: {
     flexDirection: 'row',
@@ -49,5 +61,8 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 5
   },
+  addTask: {
+    height: 550
+  }
 });
 export default Task;
